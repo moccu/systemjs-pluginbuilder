50f8a764739f4bd9807cfcd45154c88a8d890c5c
Norman Rusch <norman.rusch@moccu.com>
2016-05-24 14:50:02 +0200

    Add validation tasks

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+var
+	FILES_TO_VALIDATE = [
+		'src/**/*.js',
+		'example/**/*.js',
+		'Gruntfile.js',
+
+		// Exclude:
+		'!**/node_modules/**',
+		'!**/jspm_packages/**',
+		'!**/build/**',
+		'!**/config.js'
+	]
+;
+
+
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-lintspaces');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			all: {
+				src: FILES_TO_VALIDATE,
+				options: {
+					jshintrc: '.jshintrc'
+				}
+			}
+		},
+
+		jscs: {
+			all: {
+				src: FILES_TO_VALIDATE,
+				options: {
+					config: '.jscs.json'
+				}
+			}
+		},
+
+		lintspaces: {
+			all: {
+				src: FILES_TO_VALIDATE,
+				options: {
+					rcconfig: '.lintspacesrc'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask(
+		'validate',
+		'Validate all files.',
+		[
+			'jshint',
+			'jscs',
+			'lintspaces'
+		]
+	);
+};
